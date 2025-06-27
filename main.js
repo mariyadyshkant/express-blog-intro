6f4b14b5ce215281715b4ed9747c2b25efae0968
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-27 17:20:26 +0200

    feat: add API endpoint to retrieve blog posts

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Il server del mio blog è in ascolto su http://localhost:${port}`);
 });
+
+app.get('/api/posts', (req, res) => {
+    const posts = [
+        { id: 1, title: 'Primo Post', content: 'Contenuto del primo post', image: "./images/ciambellone.jpeg", tags: [] },
+        { id: 2, title: 'Secondo Post', content: 'Contenuto del secondo post', image: "./images/cracker_barbabietola.jpeg", tags: [] },
+        { id: 3, title: 'Terzo Post', content: 'Contenuto del terzo post', image: "./images/pane_fritto_dolce.jpeg", tags: [] },
+        { id: 4, title: 'Quarto Post', content: 'Contenuto del quarto post', image: "./images/pasta_barbabietola.jpeg", tags: [] },
+        { id: 5, title: 'Quinto Post', content: 'Contenuto del quinto post', image: "./images/torta_paesana.jpeg", tags: [] },
+
+    ];
+    res.json(posts);
+});
+
