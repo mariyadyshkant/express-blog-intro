0206baeeb50936de9b34d01888434ba35dd4f2ff
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-27 17:50:47 +0200

    fix: update image paths in posts and ensure static files are served

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.send('Server del mio blog');
 });
@@ -12,11 +14,11 @@ app.listen(port, () => {
 
 app.get('/api/posts', (req, res) => {
     const posts = [
-        { id: 1, title: 'Primo Post', content: 'Contenuto del primo post', image: "./images/ciambellone.jpeg", tags: [] },
-        { id: 2, title: 'Secondo Post', content: 'Contenuto del secondo post', image: "./images/cracker_barbabietola.jpeg", tags: [] },
-        { id: 3, title: 'Terzo Post', content: 'Contenuto del terzo post', image: "./images/pane_fritto_dolce.jpeg", tags: [] },
-        { id: 4, title: 'Quarto Post', content: 'Contenuto del quarto post', image: "./images/pasta_barbabietola.jpeg", tags: [] },
-        { id: 5, title: 'Quinto Post', content: 'Contenuto del quinto post', image: "./images/torta_paesana.jpeg", tags: [] },
+        { id: 1, title: 'Primo Post', content: 'Contenuto del primo post', image: "/ciambellone.jpeg", tags: [] },
+        { id: 2, title: 'Secondo Post', content: 'Contenuto del secondo post', image: "/cracker_barbabietola.jpeg", tags: [] },
+        { id: 3, title: 'Terzo Post', content: 'Contenuto del terzo post', image: "/pane_fritto_dolce.jpeg", tags: [] },
+        { id: 4, title: 'Quarto Post', content: 'Contenuto del quarto post', image: "/pasta_barbabietola.jpeg", tags: [] },
+        { id: 5, title: 'Quinto Post', content: 'Contenuto del quinto post', image: "/torta_paesana.jpeg", tags: [] },
 
     ];
     res.json(posts);
